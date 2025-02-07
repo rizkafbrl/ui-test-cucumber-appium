1f8315161c6fffd6e6e2642a41e64380757a590b
Rizka Febrila <rizkafbrl@gmail.com>
2025-02-07 23:58:41 +0700

    [Project] setup - add reporting to the pom.xml

--- src/test/java/com/example/RunCucumberTest.java
+++ src/test/java/com/example/RunCucumberTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.example.stepdefinitions",
-        plugin = {"pretty"}
+        plugin = {"pretty", "json:target/cucumber.json"}
 )
 public class RunCucumberTest {
 }
