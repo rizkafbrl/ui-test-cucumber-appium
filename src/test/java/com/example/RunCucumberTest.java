5685bbdc01d9e53cd8217cce9f2f4fdd53d7ae1e
Rizka Febrila <rizkafbrl@gmail.com>
2025-02-08 02:54:40 +0700

    [runner] features - set them to run on every features

--- src/test/java/com/example/RunCucumberTest.java
+++ src/test/java/com/example/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/",
         glue = "com.example.stepdefinitions",
         plugin = {"pretty", "json:target/cucumber-reports/cucumber.json"}
 )
