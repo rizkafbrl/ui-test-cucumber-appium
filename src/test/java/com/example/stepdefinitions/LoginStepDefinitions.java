00721a8ec4eafad4ce4a4130d7814d0e745b41a5
Rizka Febrila <rizkafbrl@gmail.com>
2025-02-09 15:40:45 +0700

    [testStepDefinition] Refactor as the app behaviour

--- src/test/java/com/example/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/com/example/stepdefinitions/LoginStepDefinitions.java
@@ -29,7 +29,6 @@ public class LoginStepDefinitions {
         helperCommons.setUp();
     }
 
-    // Common method to find username and password fields
     private WebElement getUsernameField() {
         WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
         return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/nameET")));
@@ -40,25 +39,15 @@ public class LoginStepDefinitions {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/passwordET")));
     }
 
-    // Common method to clear and enter text into fields
     private void enterText(WebElement field, String text) {
         field.clear();
         field.sendKeys(text);
     }
 
-    // Common method to tap the login button
     private void tapLoginButton() {
         WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
         WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.saucelabs.mydemoapp.android:id/loginBtn")));
         loginButton.click();
-    }
-
-    // Common method to check if user is redirected to the homepage
-    private void verifyHomepageRedirect() {
-        WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
-        WebElement homepageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/homepage")));
-        MobileElement homepage = (MobileElement) homepageElement; // Cast to MobileElement
-        assertThat(homepage.isDisplayed()).isTrue();
     }
 
     @When("User enters a valid username and password")
@@ -90,11 +79,6 @@ public class LoginStepDefinitions {
         tapLoginButton();
     }
 
-    @Then("User should be redirected to the homepage")
-    public void userShouldBeRedirectedToHomepage() {
-        verifyHomepageRedirect();
-    }
-
     @Then("User should see an error message under the username field")
     public void userShouldSeeErrorUnderUsernameField() {
         WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
@@ -111,39 +95,8 @@ public class LoginStepDefinitions {
 
     @Then("User should see error messages under both username and password fields")
     public void userShouldSeeErrorsUnderBothFields() {
-        WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
-        WebElement usernameError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/nameErrorTV")));
-        WebElement passwordError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/passwordErrorTV")));
-        assertThat(usernameError.isDisplayed()).isTrue();
-        assertThat(passwordError.isDisplayed()).isTrue();
-    }
-
-    @When("User is on the homepage")
-    public void userIsOnHomepage() {
-        WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
-        WebElement homepageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/homepage")));
-        assertThat(homepageElement.isDisplayed()).isTrue();
-    }
-
-    @When("User taps on the menu button")
-    public void userTapsOnMenuButton() {
-        WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
-        WebElement menuButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.saucelabs.mydemoapp.android:id/menuBtn")));
-        menuButton.click();
-    }
-
-    @Then("User should be able to see menu items")
-    public void userShouldSeeMenuItems() {
-        WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
-        WebElement menuItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.saucelabs.mydemoapp.android:id/menuItem")));
-        assertThat(menuItem.isDisplayed()).isTrue();
-    }
-
-    @When("User taps on {string} menu item")
-    public void userTapsMenuItem(String menuItemName) {
-        WebDriverWait wait = new WebDriverWait(driver, WaitTimes.QUICK_WAIT);
-        WebElement menuItem = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.TextView[@text='" + menuItemName + "']")));
-        menuItem.click();
+        userShouldSeeErrorUnderUsernameField();
+        // userShouldSeeErrorUnderPasswordField();
     }
 
     @After
